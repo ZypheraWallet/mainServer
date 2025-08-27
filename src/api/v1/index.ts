220ec4f6a36dd94c82f0baa52739917f954b7833
Kollusion <kollusion.dark@gmail.com>
2025-08-27 20:01:07 +0700

    s

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -8,7 +8,7 @@ const v1 = new Hono()
 v1.use(
     '/auth/*',
     cors({
-        origin: 'http://localhost:3001',
+        origin: 'https://web-wallet-sepia.vercel.app/',
         allowMethods: ['GET', 'POST', 'OPTIONS'],
         allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
         exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
